b9bc4e7d6321d7a4aeaae00dea41f10176cb89c6
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-11-23 21:43:31 +0100

    add RunningState class to manage player running behavior and state transitions

---
+++ src/mariopatterns/player/state/RunningState.java
@@ -0,0 +1,29 @@
+package mariopatterns.player.state;
+
+import mariopatterns.player.Player;
+
+import java.awt.event.KeyEvent;
+
+public class RunningState extends AbstractPlayerState {
+    @Override public void enter(Player player) { logger.logState("Player: RUNNING"); }
+    @Override public void update(Player player) {
+        // Mouvement horizontal simple
+        if (player.isKeyPressed(KeyEvent.VK_LEFT)) player.x -= 5;
+        if (player.isKeyPressed(KeyEvent.VK_RIGHT)) player.x += 5;
+    }
+    @Override public void handleInput(Player player) {
+        boolean left = player.isKeyPressed(KeyEvent.VK_LEFT);
+        boolean right = player.isKeyPressed(KeyEvent.VK_RIGHT);
+
+        if (!left && !right) {
+            changeState(player, new IdleState(), "IDLE");
+        }
+        if (player.isKeyPressed(KeyEvent.VK_SPACE)) {
+            changeState(player, new JumpingState(), "JUMPING");
+        }
+    }
+    @Override public void onCollisionWithEnemy(Player player) {
+        changeState(player, new DeadState(), "DEAD");
+    }
+    @Override protected String getStateName() { return "RUNNING"; }
+}
