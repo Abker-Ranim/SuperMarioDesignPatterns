ee5ab43cf90f665370fafd6ec59231b10d2a699c
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-11-23 22:00:20 +0100

    add PlayerDecorator class to enhance player functionality through composition

---
+++ src/mariopatterns/player/decorator/PlayerDecorator.java
@@ -0,0 +1,18 @@
+package mariopatterns.player.decorator;
+
+import java.awt.Graphics2D;
+
+public abstract class PlayerDecorator implements PlayerComponent {
+    protected final PlayerComponent decoratedPlayer;
+
+    public PlayerDecorator(PlayerComponent decoratedPlayer) {
+        this.decoratedPlayer = decoratedPlayer;
+    }
+
+    @Override public void update() { decoratedPlayer.update(); }
+    @Override public void render(Graphics2D g) { decoratedPlayer.render(g); }
+    @Override public int getX() { return decoratedPlayer.getX(); }
+    @Override public int getY() { return decoratedPlayer.getY(); }
+    @Override public double getSpeedMultiplier() { return decoratedPlayer.getSpeedMultiplier(); }
+    @Override public boolean isInvincible() { return decoratedPlayer.isInvincible(); }
+}
