4b2556075e307b236691e224a7e06958c9bc7aea
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-11-24 18:16:19 +0100

    add player name retrieval and final score saving in GameOverState

--- src/mariopatterns/game/state/GameOverState.java
+++ src/mariopatterns/game/state/GameOverState.java
@@ -1,11 +1,16 @@
 package mariopatterns.game.state;
 
 import mariopatterns.game.GameContext;
+import mariopatterns.ui.GamePanel;
 
 public class GameOverState extends AbstractGameState {
     @Override public void enter(GameContext context) {
         logger.logInfo("Game Over ! Score final : " + context.getScore());
+        // Récupère le nom du joueur depuis GamePanel
+        String playerName = ((GamePanel)context.getGamePanel()).getPlayerName(); // on ajoutera ça
+        context.saveFinalScore(playerName);
     }
+
     @Override public void update(GameContext context) {}
     @Override public void render(GameContext context) {}
     @Override public void handleInput(GameContext context) {}
