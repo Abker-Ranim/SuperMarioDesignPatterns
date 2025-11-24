ac957e2d04092eca6f6b15bc477aaa43f212c996
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-11-24 18:17:00 +0100

    add player animation states and rendering logic in Player class

--- src/mariopatterns/player/Player.java
+++ src/mariopatterns/player/Player.java
@@ -4,8 +4,14 @@ import mariopatterns.game.GameContext;
 import mariopatterns.player.decorator.BasePlayer;
 import mariopatterns.player.decorator.PlayerComponent;
 import mariopatterns.player.state.IdleState;
+import mariopatterns.player.state.JumpingState;
 import mariopatterns.player.state.PlayerState;
+import mariopatterns.player.state.RunningState;
+import mariopatterns.utils.ImageLoader;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,9 +23,13 @@ public class Player {
     private final Set<Integer> pressedKeys = new HashSet<>();
     public PlayerComponent renderablePlayer;
 
-    public void applyPowerUp(PlayerComponent decorator) {
-        this.renderablePlayer = decorator;
-    }
+    // TES SPRITES
+    private static final BufferedImage MARIO_STAND = ImageLoader.load("/resources/player/mario_stand.png");
+    private static final BufferedImage MARIO_RUN   = ImageLoader.load("/resources/player/mario_run.png");   // ton seul sprite run
+    private static final BufferedImage MARIO_JUMP  = ImageLoader.load("/resources/player/mario_jump.png");
+
+    private int animTimer = 0;  // pour faire "bouger" le sprite run
+
     public Player(GameContext gameContext) {
         this.gameContext = gameContext;
         currentState.enter(this);
@@ -34,16 +44,39 @@ public class Player {
     public void update() {
         currentState.update(this);
         currentState.handleInput(this);
+        animTimer++; // pour l'animation
+    }
+
+    // Ajoute cette méthode dans ta classe Player.java
+    public void render(Graphics2D g) {
+        BufferedImage currentSprite;
+
+        if (currentState instanceof mariopatterns.player.state.JumpingState) {
+            currentSprite = ImageLoader.load("/resources/player/mario_jump.png");
+        } else if (currentState instanceof mariopatterns.player.state.RunningState) {
+            currentSprite = ImageLoader.load("/resources/player/mario_run.png");
+        } else {
+            currentSprite = ImageLoader.load("/resources/player/mario_stand.png");
+        }
+
+        if (isKeyPressed(KeyEvent.VK_LEFT)) {
+            g.drawImage(currentSprite, x + 50, y, -50, 70, null);
+        } else {
+            g.drawImage(currentSprite, x, y, 50, 70, null);
+        }
+
+        // Effets power-up
+        if (renderablePlayer != null) {
+            renderablePlayer.render(g);
+        }
     }
 
     public void onCollisionWithEnemy() {
         currentState.onCollisionWithEnemy(this);
     }
 
-    // Input
     public void keyPressed(int keyCode) { pressedKeys.add(keyCode); }
     public void keyReleased(int keyCode) { pressedKeys.remove(keyCode); }
     public boolean isKeyPressed(int keyCode) { return pressedKeys.contains(keyCode); }
-
     public GameContext getGameContext() { return gameContext; }
 }
