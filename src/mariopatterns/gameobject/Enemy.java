00ab42f34b1175891f191c945543bb0a4595f0cc
Abker-Ranim <ranim.abker@enicar.ucar.tn>
2025-11-24 18:16:06 +0100

    refactor Enemy class to use ImageLoader for enemy graphics and simplify collision logic

--- src/mariopatterns/gameobject/Enemy.java
+++ src/mariopatterns/gameobject/Enemy.java
@@ -1,40 +1,36 @@
 package mariopatterns.gameobject;
 
 import mariopatterns.player.Player;
-import mariopatterns.utils.LoggerManager;
-
+import mariopatterns.utils.ImageLoader;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Enemy implements GameObject {
     public int x, y;
     private int dir = 1;
     private final Player player;
-    private final LoggerManager logger = LoggerManager.getInstance();
+    private static final BufferedImage SPINY = ImageLoader.load("/resources/enemies/spiny.png");
 
     public Enemy(int x, int y, Player player) {
-        this.x = x; this.y = y; this.player = player;
+        this.x = x;
+        this.y = y;
+        this.player = player;
     }
 
     @Override
     public void update() {
         x += dir * 2;
-        if (x < 0 || x > 800) dir *= -1;
+        if (x < 50 || x > 750) dir *= -1;
 
-        // Collision simple
-        if (Math.abs(x - player.x) < 40 && Math.abs(y - player.y) < 60) {
-            if (!player.renderablePlayer.isInvincible()) {
-                player.onCollisionWithEnemy();
-                logger.logCollision("Enemy hit player !");
-            } else {
-                logger.logInfo("Enemy hit player but SHIELD active → ignored");
-            }
+        // Collision = Game Over
+        if (Math.abs(x - player.x) < 45 && Math.abs(y - player.y) < 60) {
+            player.onCollisionWithEnemy();
         }
     }
 
     @Override
     public void render(Graphics2D g) {
-        g.setColor(Color.RED);
-        g.fillRect(x, y, 40, 40);
+        g.drawImage(SPINY, x, y, 50, 50, null);
     }
 
     @Override
